6a159d53405e35ada0e8c05c59e565af110d5a23
Rodolpho Reis <rodolphoreis@live.com>
2024-10-26 18:36:22 +0100

    chore(Transition): disable eslint exhaustive-deps warning and add Transition component with fade-in animation using framer-motion

--- src/components/Transition/Transition.tsx
+++ src/components/Transition/Transition.tsx
@@ -1,11 +1,36 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
 
 import { useEffect, useRef } from "react";
 import { TransitionProps } from "./Transition.types";
+import { useAnimation, useInView, motion } from "framer-motion";
+import { fadeIn } from "./TransitionFramer";
 
 export function Transition(props: TransitionProps) {
   const { children, className } = props;
   const ref = useRef(null);
 
-  return <div ref={ref}>{children}</div>;
+  const isInView = useInView(ref, { once: false });
+  const mainControls = useAnimation();
+  const slideControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+      slideControls.start("visible");
+    }
+  }, [isInView]);
+
+  return (
+    <div ref={ref}>
+      <motion.div
+        variants={fadeIn()}
+        initial="hidden"
+        animate={mainControls}
+        exit="hidden"
+        className={className}
+      >
+        {children}
+      </motion.div>
+    </div>
+  );
 }
