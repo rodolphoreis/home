95c90e685e410e9cdf726bf080d60b95687926f4
Rodolpho Reis <rodolphoreis@live.com>
2024-10-26 18:37:46 +0100

    feat(component): add FloatedSearch with animated transition and search components for location, property, and price range

--- src/components/FloatedSearch/FloatedSearch.tsx
+++ src/components/FloatedSearch/FloatedSearch.tsx
@@ -1,6 +1,18 @@
+import { SearchButton } from "../SearchButton";
+import { SearchLocation } from "../SearchLocation";
+import { SearchPriceRange } from "../SearchPriceRange";
+import { SearchProperty } from "../SearchProperty";
+import { Transition } from "../Transition";
 
 export function FloatedSearch() {
-    return (
-        <p>Floated search</p>
-    )
+  return (
+    <Transition className="absolute bottom-10 md:-bottom-10 left-0 right-0 w-[75%] mx-auto">
+      <div className="flex-col px-4 justify-between gap-4 py-4 bg-white rounded-md md:flex md:flex-row backdrop-blur shadow-light">
+        <SearchLocation />
+        <SearchProperty />
+        <SearchPriceRange />
+        <SearchButton />
+      </div>
+    </Transition>
+  );
 }
