12826fab1a450ddd0fd14d3362bd4d1231e377b1
Rodolpho Reis <rodolphoreis@live.com>
2024-10-26 18:35:53 +0100

    feat(Banner): integrate FloatedSearch component into banner layout for enhanced search functionality

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,5 +1,20 @@
+import { FloatedSearch } from "../FloatedSearch";
+
 export function Banner() {
-    return (
-        <p>Banner</p>
-    )
+  return (
+    <div className="container relative mx-auto">
+      <div className="pt-20 md:pt-0 min-h-[80vh] bg-banner bg-no-repeat bg-cover bg-center rounded-3xl relative flex flex-col items-center md:justify-center">
+        <div className="max-w-3xl text-center text-white">
+          <h2 className="text-4xl px-2 md:px-0 md:text-5xl font-semibold">
+            Descubra a casa ideal para tua família em todo Portugal
+          </h2>
+          <p className="mt-5 text-lg px-2 md:px-0 md:text-xl md:mt-8">
+            Com mais de 30 anos de experiência, a MyHome reúne os melhores
+            profissionais para atender a todas as suas necessidades.
+          </p>
+        </div>
+        <FloatedSearch />
+      </div>
+    </div>
+  );
 }
