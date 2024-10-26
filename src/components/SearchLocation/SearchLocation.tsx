2b168a681265095174d9e41e37461f325263001d
Rodolpho Reis <rodolphoreis@live.com>
2024-10-26 18:36:46 +0100

    feat(SearchLocation): add interactive dropdown for selecting location with toggle functionality

--- src/components/SearchLocation/SearchLocation.tsx
+++ src/components/SearchLocation/SearchLocation.tsx
@@ -1,10 +1,31 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 import { useState } from "react";
 import { GrFormDown, GrLocation, GrFormUp } from "react-icons/gr";
 
 export function SearchLocation() {
   const [isOpen, setIsOpen] = useState(false);
 
-  return <p>Search Location</p>;
+  return (
+    <div
+      className="relative mb-2 md:mb-0 flex items-center gap-4 border-[1px] rounded-lg px-3 py-2 justify-between curssor-pointer"
+      onClick={() => setIsOpen((prev) => !prev)}
+    >
+      <GrLocation />
+      <div>
+        <p>Localização</p>
+        <p className="text-xs">Selecione sua Localização</p>
+      </div>
+      {isOpen ? <GrFormUp /> : <GrFormDown />}
+
+      {isOpen && (
+        <div className="absolute top-[70px] bg-white z-50 p-4 rounded-lg shadow-light w-[230px] left-0">
+          <p>Porto</p>
+          <p>Lisboa</p>
+          <p>Guimarães</p>
+          <p>Aveiro</p>
+          <p>Setúbal</p>
+          <p>Faro</p>
+        </div>
+      )}
+    </div>
+  );
 }
