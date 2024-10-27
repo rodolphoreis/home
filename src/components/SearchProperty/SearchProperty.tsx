8aaf70a6d3554427b54b41a24ba7a6819b16674a
Rodolpho Reis <rodolphoreis@live.com>
2024-10-27 12:52:08 +0000

    feat(ui): implement static SearchProperty component with dropdown for future dynamic property types

--- src/components/SearchProperty/SearchProperty.tsx
+++ src/components/SearchProperty/SearchProperty.tsx
@@ -1,10 +1,29 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 import { useState } from "react";
 import { GrFormDown, GrHome, GrFormUp } from "react-icons/gr";
 
 export function SearchProperty() {
   const [isOpen, setIsOpen] = useState(false);
 
-  return <p>Search property </p>;
+  return (
+    <div
+      className="relative mb-2 md:mb-0 flex items-center gap-4 border-[1px] rounded-lg px-3 py-2 justify-between curssor-pointer"
+      onClick={() => setIsOpen((prev) => !prev)}
+    >
+      <GrHome />
+      <div>
+        <p>Propriedade</p>
+        <p className="text-xs">Selecione o tipo da propriedade</p>
+      </div>
+      {isOpen ? <GrFormUp /> : <GrFormDown />}
+
+      {isOpen && (
+        <div className="absolute top-[70px] bg-white z-50 p-4 rounded-lg shadow-light w-[230px] left-0">
+          <p>Apartamento</p>
+          <p>Vivenda</p>
+          <p>Quinta</p>
+          <p>Casa</p>
+        </div>
+      )}
+    </div>
+  );
 }
