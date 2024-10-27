13083fa271c63b427896944bb5023b0279b6d82d
Rodolpho Reis <rodolphoreis@live.com>
2024-10-27 12:51:35 +0000

    feat(ui): add static SearchPriceRange component with dropdown for future dynamic price range options

--- src/components/SearchPriceRange/SearchPriceRange.tsx
+++ src/components/SearchPriceRange/SearchPriceRange.tsx
@@ -1,10 +1,29 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 import { useState } from "react";
 import { GrFormDown, GrTag, GrFormUp } from "react-icons/gr";
 
 export function SearchPriceRange() {
   const [isOpen, setIsOpen] = useState(false);
 
-  return <p>Search price Range</p>;
+  return (
+    <div
+      className="relative mb-2 md:mb-0 flex items-center gap-4 border-[1px] rounded-lg px-3 py-2 justify-between curssor-pointer"
+      onClick={() => setIsOpen((prev) => !prev)}
+    >
+      <GrTag />
+      <div>
+        <p>Margem de preço</p>
+        <p className="text-xs">Selecione sua margem de preço</p>
+      </div>
+      {isOpen ? <GrFormUp /> : <GrFormDown />}
+
+      {isOpen && (
+        <div className="absolute top-[70px] bg-white z-50 p-4 rounded-lg shadow-light w-[230px] left-0">
+          <p>Qualquer preço</p>
+          <p>20.000€ - 30.000€</p>
+          <p>10.000€ - 20.000€</p>
+          <p>5.000€ - 10.000€</p>
+        </div>
+      )}
+    </div>
+  );
 }
