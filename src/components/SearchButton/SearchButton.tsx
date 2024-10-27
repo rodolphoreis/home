abec2e2759d00bb517d8efbffe1ab14a65d5e57f
Rodolpho Reis <rodolphoreis@live.com>
2024-10-27 12:50:49 +0000

    feat(ui): add SearchButton component with styled search icon and button label

--- src/components/SearchButton/SearchButton.tsx
+++ src/components/SearchButton/SearchButton.tsx
@@ -1,6 +1,9 @@
-
+import { RiSearch2Line } from "react-icons/ri";
 export function SearchButton() {
-    return (
-        <p>Search button</p>
-    )
+  return (
+    <div className="gap-4 border-[1px] rounded-lg px-14 py-2 bg-secondary flex items-center text-white justify-center">
+      <span className="font-medium">Buscar</span>
+      <RiSearch2Line />
+    </div>
+  );
 }
