a94eca031e96892305e0992b129794ebb0524506
Rodolpho Reis <rodolphoreis@live.com>
2024-10-28 16:20:12 +0000

    feat(slider): add Swiper carousel with navigation and scrollbar modules

--- src/components/Services/Slider/Slider.tsx
+++ src/components/Services/Slider/Slider.tsx
@@ -1,5 +1,38 @@
+import { Navigation, Scrollbar } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { sliderDataImages } from "./Slider.data";
+import Image from "next/image";
 export function Slider() {
-    return (
-        <p>Slide </p>
-    )
+  return (
+    <Swiper
+      slidesPerView={2.5}
+      spaceBetween={15}
+      freeMode={true}
+      navigation
+      pagination={{
+        clickable: true,
+      }}
+      scrollbar={{
+        draggable: true,
+      }}
+      modules={[Navigation, Scrollbar]}
+      className="h-[340px] md:h-[320px] w-[400px] md:[500px]"
+      grabCursor
+    >
+      {sliderDataImages.map(({ id, urlImage }) => (
+        <SwiperSlide key={id}>
+          <Image
+            src={`/assets/houses/${urlImage}`}
+            alt="House"
+            className="object-cover w-full h-[300px] rounded-2xl"
+            width={300}
+            height={700}
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 }
