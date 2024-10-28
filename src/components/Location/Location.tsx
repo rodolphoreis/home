e662c201474587c793b918284c3c3fd525cb25c3
Rodolpho Reis <rodolphoreis@live.com>
2024-10-28 19:14:48 +0000

    feat(location): add Location component with map integration

--- src/components/Location/Location.tsx
+++ src/components/Location/Location.tsx
@@ -1,9 +1,41 @@
-
-"use client"
+"use client";
+import { Map } from "leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { Transition } from "../Transition";
+import { MarkerHouse } from "./MarkerHouse";
 
 export function Location() {
+  const coordinatePoint = {
+    lat: 39.5,
+    lng: -8.0,
+  };
 
-    return (
-        <p>Location</p>
-    )
+  const centerMarker = (position: { lat: number; lng: number }, fnMap: Map) => {
+    fnMap.flyTo({
+      lat: position.lat,
+      lng: position.lng,
+    });
+  };
+
+  return (
+    <Transition className="px-4 py-8 md:px-36 md:py-44">
+      <h4 className="text-center text-secondary" id="location">
+        Localização
+      </h4>
+      <h2 className="max-w-2xl mx-auto my-4 mb-8 text-3xl font-semibold text-center">
+        Moradias disponíveis em todo Portugal, oferecendo conforto e praticidade
+        em diversas regiões para atender às suas necessidades.
+      </h2>
+      <MapContainer
+        center={coordinatePoint}
+        zoom={6}
+        scrollWheelZoom={false}
+        className=" h-[700px]"
+      >
+        <TileLayer url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}.png" />
+        <MarkerHouse selectMarker={centerMarker} />
+      </MapContainer>
+    </Transition>
+  );
 }
