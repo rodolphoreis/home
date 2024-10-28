326a454a4a57cd8300a6cda4bf2b8c6402368cc3
Rodolpho Reis <rodolphoreis@live.com>
2024-10-28 16:19:36 +0000

    chore(images): update all image extensions in sliderDataImages

--- src/components/Services/Slider/Slider.data.ts
+++ src/components/Services/Slider/Slider.data.ts
@@ -1,38 +1,38 @@
 export const sliderDataImages = [
   {
     id: 1,
-    urlImage: "house1.jpg",
+    urlImage: "house1.jpeg",
   },
   {
     id: 2,
-    urlImage: "house2.jpg",
+    urlImage: "house2.jpeg",
   },
   {
     id: 3,
-    urlImage: "house3.jpg",
+    urlImage: "house3.webp",
   },
   {
     id: 4,
-    urlImage: "house4.jpg",
+    urlImage: "house4.webp",
   },
   {
     id: 5,
-    urlImage: "house5.jpg",
+    urlImage: "house5.jpeg",
   },
   {
     id: 6,
-    urlImage: "house6.jpg",
+    urlImage: "house6.jpeg",
   },
   {
     id: 7,
-    urlImage: "house7.jpg",
+    urlImage: "house7.jpeg",
   },
   {
     id: 8,
-    urlImage: "house8.jpg",
+    urlImage: "house8.jpeg",
   },
   {
     id: 9,
-    urlImage: "house9.jpg",
+    urlImage: "house9.jpeg",
   },
 ];
