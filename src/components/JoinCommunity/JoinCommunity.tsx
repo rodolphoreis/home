c28f993e7d0d715f1507cd3a89af4954efae7165
Rodolpho Reis <rodolphoreis@live.com>
2024-10-29 17:55:03 +0000

    feat: create JoinCommunity component with community join button

--- src/components/JoinCommunity/JoinCommunity.tsx
+++ src/components/JoinCommunity/JoinCommunity.tsx
@@ -1,5 +1,18 @@
+import { Transition } from "../Transition";
+import { FaRegPaperPlane } from "react-icons/fa";
+
 export function JoinCommunity() {
-    return (
-        <p>Join community</p>
-    )
+  return (
+    <Transition className="grid items-center px-4 py-8 md:py-44 md:px-40 md:grid-cols-2 bg-secondary">
+      <h3 className="max-w-lg text-4xl font-semibold text-white">
+        Junte-se à nossa comunidade para descobrir tudo
+      </h3>
+      <div className="mx-auto mt-5 md:mx-auto">
+        <button className="flex items-center justify-between py-5 text-white transition-all duration-100 bg-black rounded-lg px-7 w-fit hover:bg-black/70">
+          <FaRegPaperPlane />
+          <span className="ml-3">Junte-se à nossa comunidade</span>
+        </button>
+      </div>
+    </Transition>
+  );
 }
