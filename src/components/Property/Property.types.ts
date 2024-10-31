eb0f58b5fd47dd719ab492d58ba719b799668eb7
Rodolpho Reis <rodolphoreis@live.com>
2024-10-31 09:34:59 +0000

    feat: define PropertyProps type for house properties structure

---
+++ src/components/Property/Property.types.ts
@@ -0,0 +1,13 @@
+export type PropertyProps = {
+  houses: {
+    id: number;
+    price: number;
+    location: string;
+    star: number;
+    bedrooms: number;
+    bathroom: number;
+    meters: number;
+    description: string;
+    image: string;
+  };
+};
