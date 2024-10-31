89af96564266eba6e5956cd675029f47cddfb713
Rodolpho Reis <rodolphoreis@live.com>
2024-10-31 09:26:38 +0000

    feat: add dynamic property page with 404 check for non-existent properties

--- src/app/properties/(property)/[page]/page.tsx
+++ src/app/properties/(property)/[page]/page.tsx
@@ -1,10 +1,25 @@
-'use client'
+"use client";
+
+import { Error404 } from "@/components/Error404";
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+import { dataProperties } from "@/components/Properties/Properties.data";
+import { Property } from "@/components/Property";
+import { TransitionPage } from "@/components/TransitionPage";
+import { useParams } from "next/navigation";
 
 export default function Page() {
-
-    return (
-        <>
-            <p>Page</p>
-        </>
-    )
+  const router = useParams();
+  const propertyId = router.page;
+  const filteredPage = dataProperties.find(
+    (property) => property.id === Number(propertyId)
+  );
+  return (
+    <>
+      <TransitionPage />
+      <Header />
+      {filteredPage ? <Property /> : <Error404 />}
+      <Footer />
+    </>
+  );
 }
