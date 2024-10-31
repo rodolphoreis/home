6224935abad035cfc17873e0c672513cb737a294
Rodolpho Reis <rodolphoreis@live.com>
2024-10-31 16:37:10 +0000

    style: adjust padding and margin-top for responsive layout

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,8 +9,8 @@ import Link from "next/link";
 
 export function Footer() {
   return (
-    <div className="px-4 py-8 bg-black/90 md:py-40 md:px-36">
-      <div className="grid gap-8 grid-cols-2 md:grid-cols.[1fr,1fr,1fr,_400px] text-white">
+    <div className="px-4 py-6 bg-black/90 md:py-6 md:px-36">
+      <div className="grid gap-8 grid-cols-2 md:grid-cols.[1fr,1fr,1fr,_400px] text-white ">
         {dataFooter.map(({ id, links }) => (
           <div key={id}>
             {links.map(({ id, name, link }) => (
@@ -20,7 +20,7 @@ export function Footer() {
             ))}
           </div>
         ))}
-        <div className="md:text-right">
+        <div className="md:text-right md:mt-[-210px]">
           <h4 className="mb-6 text-xl font-semibold">MyHome</h4>
           <p>Rua do Dev, 5</p>
           <p>Guimarães, Portugal</p>
