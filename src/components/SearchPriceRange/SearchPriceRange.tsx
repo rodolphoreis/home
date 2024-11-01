ded6db900bb16f4386010e31584d9ac172a0c1cd
Rodolpho Reis <rodolphoreis@live.com>
2024-11-01 17:50:14 +0000

    refactor: improve price range selection and update filter handling

--- src/components/SearchPriceRange/SearchPriceRange.tsx
+++ src/components/SearchPriceRange/SearchPriceRange.tsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { GrFormDown, GrTag, GrFormUp } from "react-icons/gr";
+import { Context } from "../../../context/FilterContext";
 
 export function SearchPriceRange() {
+  const { filters, setFilters } = useContext(Context);
   const [isOpen, setIsOpen] = useState(false);
+
+  function handlePriceRange(range: [number, number]) {
+    setFilters({ ...filters, priceRange: range });
+    setIsOpen(true);
+  }
 
   return (
     <div
-      className="relative mb-2 md:mb-0 flex items-center gap-4 border-[1px] rounded-lg px-3 py-2 justify-between curssor-pointer"
+      className="relative mb-2 md:mb-0 flex items-center gap-4 border-[1px] rounded-lg px-3 py-2 justify-between cursor-pointer"
       onClick={() => setIsOpen((prev) => !prev)}
     >
       <GrTag />
@@ -15,13 +22,18 @@ export function SearchPriceRange() {
         <p className="hidden md:block text-xs">Selecione sua margem de preço</p>
       </div>
       {isOpen ? <GrFormUp /> : <GrFormDown />}
-
       {isOpen && (
         <div className="absolute top-[70px] bg-white z-50 p-4 rounded-lg shadow-light w-[230px] left-0">
-          <p>Qualquer preço</p>
-          <p>20.000€ - 30.000€</p>
-          <p>10.000€ - 20.000€</p>
-          <p>5.000€ - 10.000€</p>
+          <p onClick={() => handlePriceRange([0, 1000000])}>Qualquer preço</p>
+          <p onClick={() => handlePriceRange([200000, 300000])}>
+            200.000€ - 400.000€
+          </p>
+          <p onClick={() => handlePriceRange([100000, 200000])}>
+            100.000€ - 200.000€
+          </p>
+          <p onClick={() => handlePriceRange([50000, 100000])}>
+            50.000€ - 100.000€
+          </p>
         </div>
       )}
     </div>
