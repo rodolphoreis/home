7c9b618b47c6d5af40c01616a307a60e66a81271
Rodolpho Reis <rodolphoreis@live.com>
2024-11-01 17:50:39 +0000

    refactor: improve star rating filter component and enhance UX

--- src/components/SearchProperty/SearchProperty.tsx
+++ src/components/SearchProperty/SearchProperty.tsx
@@ -1,29 +1,37 @@
-import { useState } from "react";
-import { GrFormDown, GrHome, GrFormUp } from "react-icons/gr";
+import { useContext, useState } from "react";
+import { GrFormDown, GrFormUp } from "react-icons/gr";
+import { FaRegStar } from "react-icons/fa";
+import { Context } from "../../../context/FilterContext";
 
 export function SearchProperty() {
   const [isOpen, setIsOpen] = useState(false);
+  const { filters, setFilters } = useContext(Context);
+
+  function handleFilterLocation(star: [number, number]) {
+    setFilters({ ...filters, star });
+    setIsOpen(true);
+  }
 
   return (
     <div
       className="relative mb-2 md:mb-0 flex items-center gap-4 border-[1px] rounded-lg px-3 py-2 justify-between curssor-pointer"
       onClick={() => setIsOpen((prev) => !prev)}
     >
-      <GrHome />
+      <FaRegStar size={20} />
       <div>
-        <p>Propriedade</p>
-        <p className="hidden md:block text-xs">
-          Selecione o tipo da propriedade
-        </p>
+        <p>Classificação</p>
+        <p className="hidden md:block text-xs">Selecione pelas estrelas</p>
       </div>
       {isOpen ? <GrFormUp /> : <GrFormDown />}
 
       {isOpen && (
         <div className="absolute top-[70px] bg-white z-50 p-4 rounded-lg shadow-light w-[230px] left-0">
-          <p>Apartamento</p>
-          <p>Vivenda</p>
-          <p>Quinta</p>
-          <p>Casa</p>
+          <p onClick={() => handleFilterLocation([0, 5])}>
+            Qualquer quantidade
+          </p>
+          <p onClick={() => handleFilterLocation([0, 2])}>0 - 2</p>
+          <p onClick={() => handleFilterLocation([2, 4])}>2 - 4</p>
+          <p onClick={() => handleFilterLocation([5, 10])}>5 ou mais</p>
         </div>
       )}
     </div>
