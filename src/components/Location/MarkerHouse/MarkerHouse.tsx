56d2341b7a4cd8ccbb2f8ae3d1619cc58d652319
Rodolpho Reis <rodolphoreis@live.com>
2024-11-04 15:26:14 +0000

    feat: add link to view property details page

--- src/components/Location/MarkerHouse/MarkerHouse.tsx
+++ src/components/Location/MarkerHouse/MarkerHouse.tsx
@@ -4,6 +4,7 @@ import { icon } from "leaflet";
 import { housesData } from "./MarkerHouse.data";
 import { FiMapPin } from "react-icons/fi";
 import Image from "next/image";
+import Link from "next/link";
 
 export function MarkerHouse(props: MarkerHouseProps) {
   const { selectMarker } = props;
@@ -33,12 +34,15 @@ export function MarkerHouse(props: MarkerHouseProps) {
           alt={name}
           width={100}
           height={400}
-          className="w-full object-cover md:h-[180px] rounded-lg"
+          className="w-full object-cover md:h-[180px] rounded-lg mb-3"
         />
 
-        <button className="px-3 py-2 text-xs text-white rounded-lg bg-secondary mt-3 w-full">
-          Ver propriedade
-        </button>
+        <Link href={`/properties/${id}`}>
+          <span className="text-white bg-secondary rounded-md px-3 py-1 mt-2">
+            {" "}
+            Ver propriedade
+          </span>
+        </Link>
       </Popup>
     </Marker>
   ));
